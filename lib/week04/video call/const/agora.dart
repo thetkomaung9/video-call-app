9a8e01bc9a2af22c42ea26650bb9eb8110329275
thetko1996 <thetkomaung966@gmail.com>
2025-10-05 02:15:05 +0900

    Fix: Add missing semicolon to TEMP_TOKEN constant
    This commit adds a missing semicolon to the TEMP_TOKEN constant in agora.dart, resolving a syntax error.

--- lib/week04/video call/const/agora.dart
+++ lib/week04/video call/const/agora.dart
@@ -1,3 +1,3 @@
 const App_ID = '045f08b4facf4c75bd4933d9eb45544d';
 const CHANNEL_NAME = 'Summon';
-const TEMP_TOKEN = '007eJxTYFjzcYbm1mTzAz9NbuytYu2dcWDB5LN/1/Z6at1LWfvGnvOzAoOBiWmagUWSSVpicppJsrlpUoqJpbFximVqkompqYlJyuppdzIaAhkZNgr/ZmZkgEAQn50huDQvNz8/j4EBAL/HJHA='
+const TEMP_TOKEN = '007eJxTYFjzcYbm1mTzAz9NbuytYu2dcWDB5LN/1/Z6at1LWfvGnvOzAoOBiWmagUWSSVpicppJsrlpUoqJpbFximVqkompqYlJyuppdzIaAhkZNgr/ZmZkgEAQn50huDQvNz8/j4EBAL/HJHA=';
