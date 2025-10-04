311b9171ef99f2bf6ebb17e64c640aa1a5783d33
thetko1996 <thetkomaung966@gmail.com>
2025-10-05 01:40:05 +0900

    feat: Initialize camera list in main function
    This commit initializes the camera list  by calling  within the  function. This ensures the cameras are available before the app starts.

--- lib/week04/video call/camera_example.dart
+++ lib/week04/video call/camera_example.dart
@@ -2,3 +2,9 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 late List<CameraDescription> _cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  _cameras = await availableCameras();
+  runApp(const MyApp());
+}
