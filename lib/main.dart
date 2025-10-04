7713f56ca9ea1bf3ab0aedb491dfbd1f013afb2a
thetko1996 <thetkomaung966@gmail.com>
2025-10-05 02:42:35 +0900

    feat: Initialize basic app structure with MaterialApp and HomeScreen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
 import 'package:my_video_call_app/week04/video call/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'week04/video call/screen/home_screen.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
