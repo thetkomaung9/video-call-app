df96a9e63661f750e2e655b8387ac832a2477fc2
thetko1996 <thetkomaung966@gmail.com>
2025-10-05 02:38:05 +0900

    Fix: Remove unnecessary build method from HomeScreen
    The HomeScreen widget no longer needs a build method as its functionality has been removed. This commit removes the unnecessary code.

--- lib/week04/video call/screen/home_screen.dart
+++ lib/week04/video call/screen/home_screen.dart
@@ -1,22 +1,5 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Video Call'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/call');
-          },
-          child: const Text('Join Call'),
-        ),
-      ),
-    );
-  }
+  const HomeScreen({Key? key}) : super (key: key)
 }
