42b91815ec35a5e09304cb4a6641d0c21dd1d490
thetko1996 <thetkomaung966@gmail.com>
2025-10-06 02:06:22 +0900

    Fix: Implement onJoinChannelSuccess event handler
    This commit implements the  event handler for the RTC engine. This ensures proper handling of successful channel joining events.

--- lib/week04/video call/screen/cam_screen.dart
+++ lib/week04/video call/screen/cam_screen.dart
@@ -39,7 +39,7 @@ class _CamScreenState extends State<CamScreen> {
           engine!.registerEventHandler(
 
             RtcEngineEventHandler(
-              on
+              onJoinChannelSuccess: (RtcConnection connection,)
             )
           )
         }
