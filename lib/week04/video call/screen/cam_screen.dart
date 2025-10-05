69875b9cdbb327713d200b6b62aa40c2f5252b7d
thetko1996 <thetkomaung966@gmail.com>
2025-10-06 02:11:22 +0900

    feat: Implement onLeaveChannel callback to reset UID
    This commit adds the  callback to the RtcEngineEventHandler, resetting the UID when a user leaves the channel. This ensures proper state management after leaving a video call.

--- lib/week04/video call/screen/cam_screen.dart
+++ lib/week04/video call/screen/cam_screen.dart
@@ -46,6 +46,12 @@ class _CamScreenState extends State<CamScreen> {
                   this.uid = connection.localUid;
                 });
               },
+              onLeaveChannel: (RtcConnection connection, RtcStats stats) {
+                print('채널 퇴장');
+                setState((){
+                  uid = null;
+                });
+              }
             )
           )
         }
