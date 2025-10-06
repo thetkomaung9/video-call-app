f73e195e2bca0b3df5daa057de9426f22c3722cf
thetko1996 <thetkomaung966@gmail.com>
2025-10-07 01:18:52 +0900

    Fix: Corrected import path for agora constants.

--- lib/week04/video_call/screen/cam_screen.dart
+++ lib/week04/video_call/screen/cam_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
-import 'package:video_call/const/agora.dart';
+import 'package:video_call_app/const/agora.dart';
+
 
 class CamScreen extends StatefulWidget {
   const CamScreen({Key? key}) : super(key: key);
